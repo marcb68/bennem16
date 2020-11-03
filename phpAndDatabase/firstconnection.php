f19c1fb0f26b245a41923c6c24017bebecbd9b39
marcb68 <marcb68@yahoo.com>
2020-11-04 10:23:07 +1300

    firstconnection.php

--- phpAndDatabase/firstconnection.php
+++ phpAndDatabase/firstconnection.php
@@ -1,10 +1,10 @@
 <?php
 //Create a database connection
-$server = zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
-$dbusername = kec4kj9vpzbctojt
-    $dbpassword = apwya9mj52zmgse2
-        $dbname = llbur9yycltyjt3b
-            $conn = new mysqli($server, $dbusername, dbpassword, $dbname,);
+$server = 'zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$dbusername = 'kec4kj9vpzbctojt';
+    $dbpassword = 'apwya9mj52zmgse2';
+        $dbname = 'llbur9yycltyjt3b';
+            $conn = new mysqli($server, $dbusername, $dbpassword, $dbname,);
 if ($conn->error){
     echo $conn->error;
 }else{
