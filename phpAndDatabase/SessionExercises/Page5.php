14908a0d347ff9b7fe138d36a9d14fd2af922aaf
marcb68 <marcb68@yahoo.com>
2020-11-09 09:36:33 +1300

    Session Exercise

--- phpAndDatabase/SessionExercises/Page5.php
+++ phpAndDatabase/SessionExercises/Page5.php
@@ -1,1 +1,17 @@
 <?php
+?>
+<html>
+<head>
+    <title>Session Exercises</title>
+</head>
+<body>
+<ul>
+    <li><a href="Page1.php">Page 1</a> </li>
+    <li><a href="Page2.php">Page 2</a> </li>
+    <li><a href="Page3.php">Page 3</a> </li>
+    <li><a href="Page4.php">Page 4</a> </li>
+    <li><a href="Page5.php">Page 5</a> </li>
+</ul>
+</body>
+
+</html>
